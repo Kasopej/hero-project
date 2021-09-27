8f4a9b3c4c8bf9780dea61a997233dc7584ec940
Kasope Johnson <kasopej@gmail.com>
2021-09-27 12:16:36 +0100

    Added data parsing& error checking and environment variables

--- src/shared/data.js
+++ src/shared/data.js
@@ -1,15 +1,34 @@
 import axios from 'axios';
 import { format, parseISO } from 'date-fns';
 import { inputDateFormat } from './constants';
+import { API } from "./config";
 
 let getHeroes = async function(){
-  const response = await axios.get('api/heroes.json');
-  const heroesData = response.data.map((h) => {
-    h.originDate = format(parseISO(h.originDate), inputDateFormat);
-    return h;
-  });
-  console.log(`formatted heroes data: ${heroesData}`);
-  return heroesData;
+  try {
+    const response = await axios.get(`${API}/heroes.json`);
+    let data = parseList(response);
+    const heroesData = data.map((h) => {
+      h.originDate = format(parseISO(h.originDate), inputDateFormat);
+      return h;
+      });
+    console.log(`formatted heroes data: ${heroesData}`);
+    return heroesData;
+  } 
+  catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+let parseList = (response) => {
+  if (response.status != 200) throw Error(response.message);
+  if (!response.data) return []; 
+  let list = response.data;
+  if (typeof list != 'object') {
+      list = [];
+  }
+  return list;
+
 }
 
 export const data = {
