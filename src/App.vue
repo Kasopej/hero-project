6a4978b8063b1882fc400c4434d72f1c6d947540
Kasope Johnson <kasopej@gmail.com>
2021-10-01 14:23:50 +0100

    updated project

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 <template>
-
-  <HeaderBar />
-  <h1>Heroes App</h1>
-  <div class="hero-container">
-    <Heroes />
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
+  <router-view/>
 </template>
-
-<script>
-import Heroes from "@/components/Heroes";
-import HeaderBar from "@/components/header-bar";
-
-export default {
-  name: 'App',
-  components: {
-    Heroes, HeaderBar
-  }
-}
-</script>
 
 <style>
 #app {
@@ -25,29 +12,19 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  margin: 0;
+  color: #2c3e50;
 }
 
-body, a{color: white !important; text-decoration: none;}
-body{padding: 0; margin: 0;}
-nav{display: flex; justify-content: space-between; align-items: center; background-color: blue; margin-bottom: 0; width: 100%;}
-ul{list-style: none; padding: 0;} 
-h1{text-align: start; color: black; padding-left: 10px;}
-.hero-container{width: 100%; background-color: rgb(20, 17, 51); padding: 20px;}
-.message{background-color: blue; padding: 10px; font-weight: bold; text-transform: capitalize;}
-.header-bar-links{margin: 0 10px;}
-.header-bar-links li{display: inline-block; padding: 0 5px;}
-.heroes-list-container{display: flex; flex-flow: row wrap; justify-content: space-around; padding: 10px;}
-.hero-list-group {border-radius: 3px; background-color: white; padding: 5px; width: 40%;}
-.hero-details div {padding: 8px; text-align: center;} 
-.hero-list-name{font-size: 2rem; background-color: green; border-radius: 3px; margin-bottom: 8px;}
-.hero-list-name:hover{cursor: pointer;}
-.hero-list-group li:nth-child(2){color: black;}
-.hero-details label, .hero-details input {display: block; border-radius: 2.5px; width: 200px; margin: auto; text-align: center;
-margin-bottom: 7px;}
-.actions button {margin: 0 4px;}
+#nav {
+  padding: 30px;
+}
 
-@media screen and (min-width: 992px) {
-  .hero-container{width: 60%;}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
